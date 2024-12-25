da9be11d6c1970c5f06504d7eac8e0aa567f84dd
akshitagarg19 <akshitagargg19@gmail.com>
2024-12-25 22:33:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const facts = [
     { text: 'Kevin Mitnick was once the most-wanted computer criminal in the U.S.', image: 'https://up.yimg.com/ib/th?id=OIP.nMCtxXaaItRzUFSX8tfkEQHaLH&pid=Api&rs=1&c=1&qlt=95&w=75&h=113' },
     { text: 'Hacktivism combines hacking with activism to promote political agendas.', image: 'https://up.yimg.com/ib/th?id=OIP.nMCtxXaaItRzUFSX8tfkEQHaLH&pid=Api&rs=1&c=1&qlt=95&w=75&h=113' },
     { text: 'Capture the Flag (CTF) competitions challenge participants to solve security-related puzzles.', image: 'https://up.yimg.com/ib/th?id=OIP.EhFgdU2ozfg7ZqLppSUNagHaEe&pid=Api&rs=1&c=1&qlt=95&w=161&h=97' },
-    { text: 'The dark web is often associated with illegal activities, including the sale of stolen data.', image: 'https://up.yimg.com/ib/th?id=OIP.TZVud3Pj77l11kPmmB8-8wHaFO&pid=Api&rs=1&c=1&qlt=95&w=145&h=102' },
+    { text: 'The dark web is often associated with illegal activities, including the sale of stolen data.', image: 'https://up.yimg.com/ib/th?id=OIP.z1Fl9P5QfqFRfLuPS5GjQwHaFk&pid=Api&rs=1&c=1&qlt=95&w=141&h=106' },
 ];
 
 let currentFactIndex = 0;
